c0c9ab9a2f96cbd251bc68ee0fc1f71cf011be7b
Cecilia Sánchez <cevictoria@gmail.com>
2024-01-31 20:38:46 -0300

    ahora sí!

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,7 @@
 const getProducts = require("./GetProducts");
 const getProductsCart = require("./GetProductsCart");
-const addProductCart = require("./addProductCart");
-const putProduct = require("./putProduct");
+const addProductCart = require("./AddProductCart");
+const putProduct = require("./PutProduct");
 const deleteProduct = require("./DeleteProduct");
 const getProductById = require("./GetProductById")
 
